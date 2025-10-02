1009afb9b7e6196a33b68bf0f9f85d62b5594d01
UitgehongerdeRat <97099793@st.deltio.nlm>
2025-10-02 15:44:12 +0200

    kekkerekkerkkernewrjewjr

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -5,3 +5,14 @@
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 
+import math
+
+
+zijde_1 = float(input("Geef de lengte van de eerste zijde: "))
+zijde_2 = float(input("Geef de lengte van de tweede zijde: "))
+
+
+schuine_zijde = math.sqrt(zijde_1**2 + zijde_2**2)
+
+
+print("De lengte van de schuine zijde is:", schuine_zijde)
--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,13 @@
 
 # Hier komt je code...
 
+input_str = input(['Sri Lanka', 'Rwanda', 'Oezbekistan', 'Finland', 'Chad'])
+
+
+objecten = [item.strip() for item in input_str.split(",")]
+
+
+objecten.sort(reverse=True)
+
+
+print(objecten)
